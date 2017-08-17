1ddee308e949c530b1b102b478f69f4240040f76
Andrew Larcombe <andrew@andrewl.net>
2017-08-17 14:44:17 +0100

    Adds return on error.

--- discord_notify.sh
+++ discord_notify.sh
@@ -5,6 +5,7 @@ DISCORD_WEBHOOK_URL=${DISCORD_WEBHOOK_URL:-""}
 
 if [ -z "$DISCORD_WEBHOOK_URL" ]; then
   echo "You must define DISCORD_WEBHOOK_URL in order to post a message";
+  exit 1;
 fi
 
 curl -X POST --data-urlencode 'payload_json={"username": "'"$DISCORD_USERNAME"'", "content":"'"$1"'"}' $DISCORD_WEBHOOK_URL
